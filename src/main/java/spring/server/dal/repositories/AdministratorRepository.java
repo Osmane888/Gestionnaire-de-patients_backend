de1c5a1928d2247495bae06cc547a1fd2468ce35
Osmane888 <osmane688@hotmail.com>
2024-12-04 09:05:22 +0100

    admin repo retirer

--- src/main/java/spring/server/dal/repositories/AdministratorRepository.java
+++
@@ -1,8 +0,0 @@
-package spring.server.dal.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AdministratorRepository extends JpaRepository<Administrator, UUID> {
-}
