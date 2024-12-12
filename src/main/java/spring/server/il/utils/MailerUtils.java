44a73b61f22a85d364ca75f47daf9a6fe2b20951
Osmane888 <osmane688@hotmail.com>
2024-12-12 15:57:10 +0100

    configuration du mail de l'app

--- src/main/java/spring/server/il/utils/MailerUtils.java
+++ src/main/java/spring/server/il/utils/MailerUtils.java
@@ -1,7 +1,6 @@
 package spring.server.il.utils;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
@@ -10,8 +9,7 @@ import org.thymeleaf.TemplateEngine;
 @RequiredArgsConstructor
 public class MailerUtils {
 
-    @Value("clinicease4@gmail.com")
-    private String appMailAdress;
+    private String appMailAdress = "clinicease4@gmail.com";
 
     private final JavaMailSender mailSender;
     private final TemplateEngine templateEngine;
